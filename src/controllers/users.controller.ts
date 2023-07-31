2afd4ad509e49ed244f5e908c979bd591382d55f
Trần Mạnh Cường <89560029+TranManhCuongQN@users.noreply.github.com>
2023-07-31 21:14:53 +0700

    handle error

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,20 +1,16 @@
-import { Request, Response } from 'express'
+import { Request, Response, NextFunction } from 'express'
 import { ParamsDictionary } from 'express-serve-static-core'
 import { RegisterReqBody } from '~/models/requests/user.request'
 import usersService from '~/services/users.service'
 
-export const registerController = async (req: Request<ParamsDictionary, any, RegisterReqBody>, res: Response) => {
-  try {
-    const result = await usersService.register(req.body)
-    // await databaseService.users.find({})
-    return res.json({
-      message: 'Register success',
-      result
-    })
-  } catch (error) {
-    return res.status(400).json({
-      message: 'Register failed',
-      error
-    })
-  }
+export const registerController = async (
+  req: Request<ParamsDictionary, any, RegisterReqBody>,
+  res: Response,
+  next: NextFunction
+) => {
+  const result = await usersService.register(req.body)
+  return res.json({
+    message: 'Register success',
+    result
+  })
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express'
 import databaseService from './services/database.service'
 import usersRouter from './routes/users.router'
+import { defaultErrorHandler } from './middlewares/error.middleware'
 
 const app = express()
 const port = 4040
@@ -8,6 +9,8 @@ app.use(express.json())
 
 databaseService.connect()
 app.use('/users', usersRouter)
+
+app.use(defaultErrorHandler)
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
