cbac48a02cda3ba85d4dcdb302e8f252b070e210
Trần Mạnh Cường <89560029+TranManhCuongQN@users.noreply.github.com>
2023-08-02 23:00:30 +0700

    get-me

--- src/routes/users.router.ts
+++ src/routes/users.router.ts
@@ -10,6 +10,7 @@ import {
 import { Router } from 'express'
 import {
   forgotPasswordController,
+  getMeController,
   loginController,
   logoutController,
   registerController,
@@ -25,13 +26,14 @@ const usersRouter = Router()
 
 usersRouter.post('/login', loginValidator, wrapRequestHandler(loginController))
 usersRouter.post('/register', registerValidator, wrapRequestHandler(registerController))
-
 usersRouter.post('/logout', accessTokenValidator, refreshTokenValidator, wrapRequestHandler(logoutController))
 
 usersRouter.post('/verify-email', emailVerifyTokenValidator, wrapRequestHandler(verifyEmailController))
 
 // đăng nhập xong thì gửi email verify nên phải cần kiểm tra access token
 usersRouter.post('/resend-verify-email', accessTokenValidator, wrapRequestHandler(resendVerifyEmailController))
+
+usersRouter.get('/me', accessTokenValidator, wrapRequestHandler(getMeController))
 
 usersRouter.post('/forgot-password', forgotPasswordValidator, wrapRequestHandler(forgotPasswordController))
 usersRouter.post(
--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -211,6 +211,21 @@ class UsersService {
       message: USERS_MESSAGES.RESET_PASSWORD_SUCCESS
     }
   }
+
+  // projection : {password: 0} => không lấy password
+  async getMe(user_id: string) {
+    const user = await databaseService.users.findOne(
+      { _id: new ObjectId(user_id) },
+      {
+        projection: {
+          password: 0,
+          email_verify_token: 0,
+          forgot_password_token: 0
+        }
+      }
+    )
+    return user
+  }
 }
 
 const usersService = new UsersService()
