c29cc03617a73928828bec96df5bc395a0447e7c
Trần Mạnh Cường <89560029+TranManhCuongQN@users.noreply.github.com>
2023-08-07 11:44:05 +0700

    upload file with formidable

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,20 @@ import express, { Request, Response, NextFunction } from 'express'
 import databaseService from './services/database.service'
 import usersRouter from './routes/users.router'
 import { defaultErrorHandler } from './middlewares/error.middleware'
+import mediasRouter from './routes/medias.router'
+import { initFolder } from './utils/file'
 
 const app = express()
 const port = 4040
 app.use(express.json())
 
 databaseService.connect()
+
+//  tao folder upload
+initFolder()
+
 app.use('/users', usersRouter)
+app.use('/medias', mediasRouter)
 
 app.use(defaultErrorHandler)
 
