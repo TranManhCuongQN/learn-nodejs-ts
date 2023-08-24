156a07acef020ddf8be0de474600cd152b04f966
Trần Mạnh Cường <89560029+TranManhCuongQN@users.noreply.github.com>
2023-08-25 01:28:26 +0700

    handle upload image multiple

--- src/services/medias.service.ts
+++ src/services/medias.service.ts
@@ -1,24 +1,34 @@
 import { Request } from 'express'
-import { getNameFromFullname, handleUploadSingleImage } from '~/utils/file'
+import { getNameFromFullname, handleUploadImage } from '~/utils/file'
 import sharp from 'sharp'
 import { UPLOAD_DIR } from '~/constants/dir'
 import path from 'path'
 import fs from 'fs'
 import { isProduction } from '~/constants/config'
+import { MediaType } from '~/constants/enum'
+import { Media } from '~/models/Other'
 import { config } from 'dotenv'
 
 class MediasService {
-  async handleUploadSingleImage(req: Request) {
-    const file = await handleUploadSingleImage(req)
-    const newName = getNameFromFullname(file.newFilename)
-    const newPath = path.resolve(UPLOAD_DIR, `${newName}.jpg`)
-    await sharp(file.filepath).jpeg().toFile(newPath)
+  async uploadImage(req: Request) {
+    const files = await handleUploadImage(req)
+    const result: Media[] = await Promise.all(
+      files.map(async (file) => {
+        const newName = getNameFromFullname(file.newFilename)
+        const newPath = path.resolve(UPLOAD_DIR, `${newName}.jpg`)
+        await sharp(file.filepath).jpeg().toFile(newPath)
 
-    // xóa file ảnh gốc trong thư mục temp
-    fs.unlinkSync(file.filepath)
-    return isProduction
-      ? `${process.env.HOST}/static/image/${newName}.jpg`
-      : `http://localhost:${process.env.PORT}/static/image/${newName}.jpg`
+        // xóa file ảnh gốc trong thư mục temp
+        fs.unlinkSync(file.filepath)
+        return {
+          url: isProduction
+            ? `${process.env.HOST}/static/image/${newName}.jpg`
+            : `http://localhost:${process.env.PORT}/static/image/${newName}.jpg`,
+          type: MediaType.Image
+        }
+      })
+    )
+    return result
   }
 }
 
