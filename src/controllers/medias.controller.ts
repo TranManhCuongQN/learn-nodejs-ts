ef1b76261b77c9316cd2825979f57050f36de19c
Trần Mạnh Cường <89560029+TranManhCuongQN@users.noreply.github.com>
2023-08-24 23:40:31 +0700

    handle image with sharp

--- src/controllers/medias.controller.ts
+++ src/controllers/medias.controller.ts
@@ -1,10 +1,9 @@
 import { NextFunction, Request, Response } from 'express'
-import path from 'path'
-import { handleUploadSingleImage } from '~/utils/file'
+import mediasService from '~/services/medias.service'
 
 export const uploadSingleImageController = async (req: Request, res: Response, next: NextFunction) => {
-  const data = await handleUploadSingleImage(req)
+  const result = await mediasService.handleUploadSingleImage(req)
   return res.json({
-    result: data
+    result: result
   })
 }
