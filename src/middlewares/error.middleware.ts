504799e4048150855e71f32cccb3eff7b86286c1
Trần Mạnh Cường <89560029+TranManhCuongQN@users.noreply.github.com>
2024-02-11 09:32:11 +0700

    fix error middleware

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -3,15 +3,34 @@ import { omit } from 'lodash'
 import HTTP_STATUS from '~/constants/httpStatus'
 import { ErrorWithStatus } from '~/models/error'
 
+// middleware này xử lý lỗi khi có lỗi xảy ra trong quá trình xử lý request toan bộ các lỗi sẽ được xử lý ở đây
+// next(err) sẽ chuyển đến middleware này để xử lý lỗi
+// (err: any, req: Request, res: Response, next: NextFunction) có dạng này để express biết đây là middleware xử lý lỗi
 export const defaultErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
-  if (err instanceof ErrorWithStatus) {
-    return res.status(err.status).json(omit(err, ['status']))
+  try {
+    if (err instanceof ErrorWithStatus) {
+      return res.status(err.status).json(omit(err, ['status']))
+    }
+    const finalError: any = {}
+
+    Object.getOwnPropertyNames(err).forEach((key) => {
+      // key $response  nó set configurable = false, writable = false nên không thể sửa đổi enumrable là true được (key $response này là từ ses của amazon sdk trả về mặc định có configurable = false, writable = false nên không thể sửa đổi được)
+      if (
+        !Object.getOwnPropertyDescriptor(err, key)?.configurable ||
+        !Object.getOwnPropertyDescriptor(err, key)?.writable
+      ) {
+        return
+      }
+      finalError[key] = err[key]
+    })
+    res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
+      message: finalError.message,
+      errorInfo: omit(finalError, ['stack'])
+    })
+  } catch (error) {
+    res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
+      message: 'Internal server error',
+      errorInfo: omit(error as any, ['stack'])
+    })
   }
-  Object.getOwnPropertyNames(err).forEach((key) => {
-    Object.defineProperty(err, key, { enumerable: true })
-  })
-  res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
-    message: err.message,
-    errorInfo: omit(err, ['stack'])
-  })
 }
