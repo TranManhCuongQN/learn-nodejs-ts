33d867236f0be462bf9054aaa7aafa760b168747
Trần Mạnh Cường <89560029+TranManhCuongQN@users.noreply.github.com>
2024-02-18 22:15:37 +0700

    middleware server

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,12 @@ import { Server } from 'socket.io'
 import Conversation from './models/schemas/conversation.schema'
 import { ObjectId } from 'mongodb'
 import conversationsRouter from './routes/conversation.router'
+import { verifyAccessToken } from '~/utils/commons'
+import { TokenPayload } from '~/models/requests/user.request'
+import { UserVerifyStatus } from '~/constants/enum'
+import { ErrorWithStatus } from '~/models/error'
+import { USERS_MESSAGES } from '~/constants/messages'
+import HTTP_STATUS from '~/constants/httpStatus'
 // import '~/utils/fake'
 
 const app = express()
@@ -62,13 +68,39 @@ const users: {
   }
 } = {}
 
+// register middleware cho socket (run trước khi connection)
+io.use(async (socket, next) => {
+  const { Authorization } = socket.handshake.auth
+  const access_token = Authorization?.split(' ')[1]
+  try {
+    const decoded_authorization = await verifyAccessToken(access_token)
+    const { verify } = decoded_authorization as TokenPayload
+    if (verify !== UserVerifyStatus.Verified) {
+      throw new ErrorWithStatus({
+        message: USERS_MESSAGES.USER_NOT_VERIFIED,
+        status: HTTP_STATUS.FORBIDDEN
+      })
+    }
+    // Truyền decoded_authorization vào socket để sử dụng ở các middleware khác
+    socket.handshake.auth.decoded_authorization = decoded_authorization
+    next()
+  } catch (error) {
+    next({
+      message: 'Unauthorized',
+      name: 'UnauthorizedError',
+      data: error
+    })
+  }
+})
+
 // khi client connect success thì cái callback này sẽ được gọi (socket) => { ... }
 // socket.on là lắng nghe sự kiện từ client gửi lên
 // socket.emit là gửi sự kiện từ server xuống client
 io.on('connection', (socket) => {
   console.log(`user ${socket.id} connected`)
 
-  const user_id = socket.handshake.auth._id
+  const { user_id } = socket.handshake.auth.decoded_authorization as TokenPayload
+
   // {'user_id': {socket_id: 'socket_id'}}
   // {
   //   '65c0b8adda0742a7dad804dd': { socket_id: '-E07swDikIPBapmHAAA8' },
@@ -79,14 +111,11 @@ io.on('connection', (socket) => {
   }
   console.log(users)
   socket.on('send_message', async (data) => {
+    console.log(data)
     const { receiver_id, sender_id, content } = data.payload
 
     // lấy socket_id của người nhận
     const receiver_socket_id = users[receiver_id]?.socket_id
-
-    if (!receiver_socket_id) {
-      return
-    }
 
     const conversation = new Conversation({
       sender_id: new ObjectId(sender_id),
@@ -96,9 +125,11 @@ io.on('connection', (socket) => {
 
     const result = await databaseService.conversations.insertOne(conversation)
     conversation._id = result.insertedId
-    socket.to(receiver_socket_id).emit('receive_message', {
-      payload: conversation
-    })
+    if (receiver_socket_id) {
+      socket.to(receiver_socket_id).emit('receive_message', {
+        payload: conversation
+      })
+    }
   })
 
   socket.on('disconnect', () => {
